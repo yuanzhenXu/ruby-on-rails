47ca46c03bd8857fe75bea33168878d66afb2408
yuanzhenXu <479359453@qq.com>
2017-03-13 23:39:28 +0800

    修改布局

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,39 +1,44 @@
 class ArticlesController < ApplicationController
    http_basic_authenticate_with name: "xu", password: "123456", except: [:index, :show]
   def update
-  @article=Article.find(params[:id])
-  if @article.update(article_params)
-    redirect_to@article
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+       redirect_to@article
     else 
       render 'edit'
-      end
+    end
   end
 
-   def new
+  def new
      @article=Article.new
-   end
-   def show
+  end
+
+  def show
      @article=Article.find(params[:id])
-   end
-   def edit
+  end
+
+  def edit
      @article=Article.find(params[:id])
-   end
-   def create
-     @article=Article.new(article_params)
-     if @article.save
-       redirect_to @article
-     else
-       render 'new'
-     end
-   end
-   def index
-     @articles=Article.all
-   end
-   def destroy
-     @article=Article.find(params[:id])
-     @article.destroy
-     redirect_to articles_path
-   end
+  end
+
+  def create
+    @article=Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @articles=Article.all
+  end
+
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
     def article_params

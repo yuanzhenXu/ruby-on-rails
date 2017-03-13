b7a6ae6ac78528f3d21d16c10512764a3d9d448b
yuanzhenXu <479359453@qq.com>
2017-03-13 21:14:08 +0800

    添加样式文件

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+   http_basic_authenticate_with name: "xu", password: "123456", except: [:index, :show]
   def update
   @article=Article.find(params[:id])
   if @article.update(article_params)
@@ -7,34 +7,36 @@ class ArticlesController < ApplicationController
     else 
       render 'edit'
       end
-      end
+  end
+
    def new
      @article=Article.new
-end
- def show
- @article=Article.find(params[:id])
-end
-    def edit
-      @article=Article.find(params[:id])
-end
-  def create 
-    @article=Article.new(article_params)
-    if @article.save
-    redirect_to@article
-    else
-  render 'new'
-    end
- end
-    def index
-   @articles=Article.all
    end
-  def destroy
-  @article=Article.find(params[:id])
-  @article.destroy
-  redirect_to articles_path
-  end
+   def show
+     @article=Article.find(params[:id])
+   end
+   def edit
+     @article=Article.find(params[:id])
+   end
+   def create
+     @article=Article.new(article_params)
+     if @article.save
+       redirect_to @article
+     else
+       render 'new'
+     end
+   end
+   def index
+     @articles=Article.all
+   end
+   def destroy
+     @article=Article.find(params[:id])
+     @article.destroy
+     redirect_to articles_path
+   end
+
   private
-   def article_params
-   params.require(:article).permit(:title,:text)
-   end
+    def article_params
+      params.require(:article).permit(:title,:text)
+    end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    http_basic_authenticate_with name: "xu", password: "123456", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -11,6 +11,7 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
+
     private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
